34d3dbd91c41f247606f865a0f90c6c10e95f586
Jordan Sissel <jls@semicomplete.com>
2012-02-08 15:01:16 -0800

    threadsafety

--- lib/ftw/pool.rb
+++ lib/ftw/pool.rb
@@ -1,19 +1,25 @@
 require "ftw/namespace"
+require "thread"
 
 class FTW::Pool
   def initialize
     # Pool is a hash of arrays.
     @pool = Hash.new { |h,k| h[k] = Array.new }
+    @lock = Mutex.new
   end # def initialize
 
   def add(identifier, object)
-    @pool[identifier] << object
+    @lock.synchronize do
+      @pool[identifier] << object
+    end
     return object
   end # def add
 
   def fetch(identifier, &default_block)
-    object = @pool[identifier].find { |o| o.available? }
-    return object if !object.nil?
+    @lock.synchronize do
+      object = @pool[identifier].find { |o| o.available? }
+      return object if !object.nil?
+    end
     # Otherwise put the return value of default_block in the
     # pool and return it.
     return add(identifier, default_block.call)
