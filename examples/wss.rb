f2508260d7dc62f436345b817e9a436d7b1ddff4
Jordan Sissel <jls@semicomplete.com>
2012-02-13 15:05:45 -0800

    - show websocket logging example also logging to stdout.

--- examples/wss.rb
+++ examples/wss.rb
@@ -1,24 +1,23 @@
 require "rubygems"
 require "eventmachine"
 require "em-websocket"
+require "cabin"
+require "logger"
+
+logger = Cabin::Channel.new
+logger.subscribe(Logger.new(STDOUT))
 
 EventMachine.run do
   EventMachine::WebSocket.start(:host => "0.0.0.0", :port => 8081) do |ws|
     ws.onopen do |*args|
       p :onopen => self, :args => args
       puts "WebSocket connection open"
-
-      # publish message to the client
-      EM::PeriodicTimer.new(3) do
-        ws.send "Hello Client"
-      end
     end
 
     ws.onclose { puts "Connection closed" }
     ws.onmessage do |msg|
-      p :onmsg => self
-      puts "Recieved message: #{msg}"
-      ws.send msg
+      data = JSON.parse(msg)
+      logger.info(data)
     end
   end
 end
