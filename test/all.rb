751c42b9eec748b277f7a563b9ba513bb1428162
Jordan Sissel <jls@semicomplete.com>
2012-02-13 23:19:07 -0800

    Add documentation tests

--- test/all.rb
+++ test/all.rb
@@ -9,8 +9,15 @@ SimpleCov.start
 # Add '../lib' to the require path.
 $: << File.join(File.dirname(__FILE__), "..", "lib")
 
-glob = File.join(File.dirname(__FILE__), "ftw", "**", "*.rb")
-Dir.glob(glob).each do |path|
+def use(path)
   puts "Loading tests from #{path}"
   require File.expand_path(path)
+end
+
+dirname = File.dirname(__FILE__)
+use File.join(dirname, "docs.rb")
+
+glob = File.join(dirname, "ftw", "**", "*.rb")
+Dir.glob(glob).each do |path|
+  use path
 end
