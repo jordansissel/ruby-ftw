19e32e7402771b2670b24efbbc9e50d8eb55528f
Jordan Sissel <jls@semicomplete.com>
2012-03-05 17:31:11 -0800

    add more doc comments

--- lib/ftw/singleton.rb
+++ lib/ftw/singleton.rb
@@ -12,6 +12,8 @@ require "ftw/namespace"
 #
 # foo = Foo.singleton
 module FTW::Singleton
+  # This is invoked when you include this module. It raises an exception because you should be
+  # using 'extend' not 'include' for this module..
   def self.included(klass)
     raise ArgumentError.new("In #{klass.name}, you want to use 'extend #{self.name}', not 'include ...'")
   end # def included
