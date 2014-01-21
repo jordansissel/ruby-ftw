ee5abf98fc0395d963614830967fb6446e925cc7
Jordan Sissel <jls@semicomplete.com>
2014-01-21 15:57:03 -0800

    Add spec that queries howsmyssl.com and fails if we are "Bad"

--- spec/ftw-agent_spec.rb
+++ spec/ftw-agent_spec.rb
@@ -28,5 +28,19 @@ describe "FTW Agent for client request" do
       response.read_body
     end
   end
+
+  context "ssl strength" do
+    let (:agent) { FTW::Agent.new }
+
+    it "should pass howsmyssl's tests" do
+      response = agent.get!("https://www.howsmyssl.com/a/check")
+      reject { response }.error?
+      payload = response.read_body
+      require "json"
+      result = JSON.parse(payload)
+      insist { result["beast_vuln"] } == false
+      insist { result["rating"] } != "Bad"
+    end
+  end
 end
 
